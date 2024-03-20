e56d990c1846d1826f5fdb8b6cca1298519b1852
Yonathan Cherkos <yonycherkos@gmail.com>
2024-03-20 21:24:38 +0300

    Remove Even Integers from an Array

--- array/ArrayUtils.java
+++ array/ArrayUtils.java
@@ -19,8 +19,31 @@ public class ArrayUtils {
         printArray(myArray);
     }
 
+    public int[] removeEven(int[] arr) {
+        int oddCount = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                oddCount++;
+            }
+        }
+
+        int[] result = new int[oddCount];
+        int idx = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                result[idx] = arr[i];
+                idx++;
+            }
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         ArrayUtils arrayUtils = new ArrayUtils();
-        arrayUtils.arrayDemo();
+        // arrayUtils.arrayDemo();
+        int[] myArray = { 3, 4, 5, 6, 7, 8 };
+        int[] result = arrayUtils.removeEven(myArray);
+        arrayUtils.printArray(result);
     }
 }
