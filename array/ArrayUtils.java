a1d06f67f90078771137f3893465ef4a6edcbbcb
Yonathan Cherkos <yonycherkos@gmail.com>
2024-03-21 19:37:52 +0300

    Move Zeroes to end

--- array/ArrayUtils.java
+++ array/ArrayUtils.java
@@ -1,24 +1,6 @@
 package array;
 
 public class ArrayUtils {
-
-    public void printArray(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-    }
-
-    public void arrayDemo() {
-        int[] myArray = new int[5];
-        myArray[0] = 5;
-        myArray[1] = 6;
-        myArray[2] = 7;
-        myArray[3] = 8;
-        myArray[4] = 9;
-        printArray(myArray);
-    }
-
     public int[] removeEven(int[] arr) {
         int oddCount = 0;
         for (int i = 0; i < arr.length; i++) {
@@ -76,14 +58,39 @@ public class ArrayUtils {
         return secondMax;
     }
 
+    public int[] moveZeros(int[] arr) {
+        int j = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[j] == 0 && arr[i] != 0) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+
+            if (arr[j] != 0) {
+                j++;
+            }
+        }
+
+        return arr;
+    }
+
+    public void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public void arrayDemo() {
+        int[] arr = { 8, 1, 0, 2, 1, 0, 3 };
+        printArray(arr);
+        moveZeros(arr);
+        printArray(arr);
+    }
+
     public static void main(String[] args) {
-        int[] myArray = { 3, 10, 5, 1, 7, 11 };
         ArrayUtils arrayUtils = new ArrayUtils();
-        // arrayUtils.arrayDemo();
-        // int[] result = arrayUtils.removeEven(myArray)
-        // arrayUtils.printArray(result);
-        // int[] result = arrayUtils.reverseArray(myArray, 0, 3);
-        // arrayUtils.printArray(result);
-        System.out.println(arrayUtils.findSecondMax(myArray));
+        arrayUtils.arrayDemo();
     }
 }
