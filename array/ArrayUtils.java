54e02d01a31163f8bb4f9078b43010dcc0add820
Yonathan Cherkos <yonycherkos@gmail.com>
2024-03-21 19:02:38 +0300

    Find Second Maximum value

--- array/ArrayUtils.java
+++ array/ArrayUtils.java
@@ -61,14 +61,29 @@ public class ArrayUtils {
         return min;
     }
 
+    public int findSecondMax(int[] arr) {
+        int secondMax = Integer.MIN_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                secondMax = max;
+                max = arr[i];
+            } else if (arr[i] > secondMax && arr[i] < max) {
+                secondMax = arr[i];
+            }
+        }
+
+        return secondMax;
+    }
+
     public static void main(String[] args) {
-        int[] myArray = { 3, 4, 5, 1, 7, 8 };
+        int[] myArray = { 3, 10, 5, 1, 7, 11 };
         ArrayUtils arrayUtils = new ArrayUtils();
         // arrayUtils.arrayDemo();
         // int[] result = arrayUtils.removeEven(myArray)
         // arrayUtils.printArray(result);
         // int[] result = arrayUtils.reverseArray(myArray, 0, 3);
         // arrayUtils.printArray(result);
-        System.out.println(arrayUtils.findMin(myArray));
+        System.out.println(arrayUtils.findSecondMax(myArray));
     }
 }
