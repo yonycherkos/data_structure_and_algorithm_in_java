232319222c5a9c69e73cd91d76cb1fa4021ed754
yonycherkos <45253598+yonycherkos@users.noreply.github.com>
2024-03-28 19:23:38 +0300

    Implement stack push and pop operation

--- stack/Stack.java
+++ stack/Stack.java
@@ -3,12 +3,48 @@ package stack;
 public class Stack {
     private int[] arr;
     private int top;
-    private int size;
+    private int capacity;
 
     public Stack(int size) {
         this.arr = new int[size];
         this.top = -1;
-        this.size = size;
+        this.capacity = size;
+    }
+
+    public void push(int data) {
+        if (isFull()) {
+            throw new StackOverflowError("Stack is full");
+        }
+        top++;
+        arr[top] = data;
+    }
+
+    public int pop() {
+        if (isEmtpy()) {
+            throw new StackOverflowError("Stack is empty");
+        }
+        int result = arr[top];
+        top--;
+        return result;
+    }
+
+    public int peak() {
+        if (isEmtpy()) {
+            throw new StackOverflowError("Stack is empty");
+        }
+        return arr[top];
+    }
+
+    public boolean isEmtpy() {
+        return top == -1;
+    }
+
+    public boolean isFull() {
+        return capacity - 1 == top;
+    }
+
+    public int size() {
+        return capacity;
     }
 
     public static void main(String[] args) {
