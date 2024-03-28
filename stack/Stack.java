0d8c6f43baacdc64317608398538c5925b897ca3
yonycherkos <45253598+yonycherkos@users.noreply.github.com>
2024-03-28 19:18:36 +0300

    Implement stack DS using array

---
+++ stack/Stack.java
@@ -0,0 +1,16 @@
+package stack;
+
+public class Stack {
+    private int[] arr;
+    private int top;
+    private int size;
+
+    public Stack(int size) {
+        this.arr = new int[size];
+        this.top = -1;
+        this.size = size;
+    }
+
+    public static void main(String[] args) {
+    }
+}
