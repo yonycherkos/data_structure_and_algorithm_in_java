f4aef623b60b2776c3d930c3118f578c0387aa99
yonycherkos <45253598+yonycherkos@users.noreply.github.com>
2024-03-30 10:53:07 +0300

    Implement next greater in array

--- stack/StackUtils.java
+++ stack/StackUtils.java
@@ -16,9 +16,28 @@ public class StackUtils {
         return new String(chars);
     }
 
+    public int[] nextGreaters(int[] arr) {
+        int[] result = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            int nextGreater = -1;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] < arr[j]) {
+                    nextGreater = arr[j];
+                    break;
+                }
+            }
+            result[i] = nextGreater;
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         StackUtils stackUtils = new StackUtils();
-        String str = "Hello World!";
-        System.out.println(stackUtils.reverseString(str));
+        int[] arr = { 4, 7, 3, 4, 8, 1 };
+        int[] result = stackUtils.nextGreaters(arr);
+        System.out.println();
+        for (int i : result) {
+            System.out.print(i + " ");
+        }
     }
 }
